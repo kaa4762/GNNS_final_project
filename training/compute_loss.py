1391e79d34c2936bb1b2616081df03072a0573ee
Kat <katharina.altrichter@gmx.net>
2025-03-04 13:49:54 +0100

    fix image to tensor transfprm

--- training/compute_loss.py
+++ training/compute_loss.py
@@ -1,9 +1,26 @@
 import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torch
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu' 
 
 def compute_losses(img_neutral, img_interpolated, img_stylized, text_neutral_emb, text_stylized_emb):
     """
     Computes CLIP loss + perceptual loss for disentanglement training.
     """
+
+    # Define transformation
+    to_tensor = transforms.ToTensor()
+
+    # Convert images to tensors if they are not already
+    if not isinstance(img_neutral, torch.Tensor):
+        img_neutral = to_tensor(img_neutral).to(DEVICE)
+
+    if not isinstance(img_interpolated, torch.Tensor):
+        img_interpolated = to_tensor(img_interpolated).to(DEVICE)
+
+    if not isinstance(img_stylized, torch.Tensor):
+        img_stylized = to_tensor(img_stylized).to(DEVICE)
+
     # Compute CLIP loss
     def clip_loss(img_emb_neutral, img_emb_interpolated, img_emb_stylized, text_neutral_emb, text_stylized_emb):
         direction_text = text_stylized_emb - text_neutral_emb
